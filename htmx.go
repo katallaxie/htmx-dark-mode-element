0b0e2d094ccb82ba6af9ffec384fd4195f103032
Sebastian Döll <sebastian@katallaxie.dev>
2026-01-02 13:08:42 +0000

    chore: add elements

---
+++ htmx.go
@@ -0,0 +1,20 @@
+package darkmode
+
+import (
+	htmx "github.com/katallaxie/htmx"
+)
+
+// DarkMode is the dark mode component.
+func DarkMode() htmx.Node {
+	return htmx.CustomElement("htmx-dark-mode")
+}
+
+// DarkTheme sets the dark theme for the dark mode component.
+func DarkTheme(theme string) htmx.Node {
+	return htmx.Attribute("dark-theme", theme)
+}
+
+// LightTheme sets the light theme for the dark mode component.
+func LightTheme(theme string) htmx.Node {
+	return htmx.Attribute("light-theme", theme)
+}
